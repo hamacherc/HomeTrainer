53a56152790ed5a629d25964e376459d9541cc59
chris <christoph.hamacher@outlook.de>
2016-02-18 20:45:18 +0100

    minor corrections

--- app/src/main/java/de/heimbeeren/hometrainer/UserLoginActivity.java
+++ app/src/main/java/de/heimbeeren/hometrainer/UserLoginActivity.java
@@ -12,6 +12,12 @@ import android.widget.Toast;
 
 import org.apache.http.params.HttpParams;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class UserLoginActivity extends Activity {
 
@@ -61,9 +67,24 @@ public class UserLoginActivity extends Activity {
         @Override
         protected String doInBackground(String... params) {
 
-
             return null;
         }
+    }
+
+    private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        for(Map.Entry<String, String> entry : params.entrySet()) {
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+        }
+        return result.toString();
     }
 
 
