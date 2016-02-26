add33c65abd2058078e4d7c9699d52ed7c409258
chris <christoph.hamacher@outlook.de>
2016-02-26 13:44:15 +0100

    Created login Code

--- app/src/main/java/de/heimbeeren/hometrainer/UserLoginActivity.java
+++ app/src/main/java/de/heimbeeren/hometrainer/UserLoginActivity.java
@@ -1,14 +1,18 @@
 package de.heimbeeren.hometrainer;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class UserLoginActivity extends Activity {
+import java.util.HashMap;
+
+public class UserLoginActivity extends Activity implements View.OnClickListener {
 
     protected EditText edtUsername;
     private EditText edtPassword;
@@ -18,7 +22,7 @@ public class UserLoginActivity extends Activity {
 
     //private final String serverUrl = "http://httpbin.org/post";
 
-    private final String serverUrl = "http://hometrainer.heimbeeren.de/android_user_api";
+    private static final String LOGIN_URL="http://hometrainer.heimbeeren.de/login.php";
     private final static String TAG = UserLoginActivity.class.getSimpleName();
 
 
@@ -27,9 +31,67 @@ public class UserLoginActivity extends Activity {
     super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         initializeViews();
-        butLogin.setOnClickListener(new View.OnClickListener() {
+    }
+
+    private void initializeViews() {
+        edtUsername = (EditText) findViewById(R.id.edt_username);
+        edtPassword = (EditText) findViewById(R.id.edt_password);
+        butLogin = (Button) findViewById(R.id.but_login);
+        butRegister = (Button) findViewById(R.id.but_register);
+    }
+
+    private void registerUser() {
+        String username = edtUsername.getText().toString().trim().toLowerCase();
+        String password = edtPassword.getText().toString().trim().toLowerCase();
+
+        login(username, password);
+    }
+
+    private void login(String username, String password) {
+        class LoginUser extends AsyncTask<String, Void, String> {
+            ProgressDialog loading;
+            RegisterUserClass ruc = new RegisterUserClass();
+
             @Override
-            public void onClick(View v) {
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(UserLoginActivity.this, "Please Wait", null, true, true);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                HashMap<String, String> data = new HashMap<String, String>();
+                data.put("name", params[0]);
+                data.put("username", params[1]);
+                data.put("password", params[2]);
+                data.put("email", params[3]);
+
+                String result = ruc.sendPostRequest(LOGIN_URL, data);
+
+                return result;
+            }
+        }
+
+        LoginUser ru = new LoginUser();
+        ru.execute(username,password);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.but_register:
+                Intent intent = new Intent(UserLoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.but_login:
                 enteredUsername = edtUsername.getText().toString();
                 String enteredPassword = edtPassword.getText().toString();
                 if (enteredUsername.equals("") || enteredPassword.equals("")) {
@@ -41,22 +103,7 @@ public class UserLoginActivity extends Activity {
                     Toast.makeText(UserLoginActivity.this, getString(R.string.hint_user_pass_min_length),
                             Toast.LENGTH_SHORT).show();
                 }
-            }
-        });
-
-        butRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(UserLoginActivity.this, RegisterActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
-
-    private void initializeViews() {
-        edtUsername = (EditText) findViewById(R.id.edt_username);
-        edtPassword = (EditText) findViewById(R.id.edt_password);
-        butLogin = (Button) findViewById(R.id.but_login);
-        butRegister = (Button) findViewById(R.id.but_register);
+                break;
+        }
     }
 }
